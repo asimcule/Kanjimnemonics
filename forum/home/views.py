66e416d4786603d7c1517d41b590d1e352e0a1e7
asimcule <asimcule11@gmail.com>
2023-08-23 00:03:12 +0545

    Bug fixes and code refactoring

--- forum/home/views.py
+++ forum/home/views.py
@@ -8,7 +8,7 @@ from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 import logging
 
-# Create your views here.
+
 @login_required(login_url='login')
 def post(request):
     form = Postform()
@@ -25,7 +25,10 @@ def post(request):
             post.save()
         else:
             messages.error(request, "No kanji found")
-            return render(request, 'home/post.html', {'form': form})
+            context_dictionary = {
+                'form': form
+            }
+            return render(request, 'home/post.html', context_dictionary)
         return redirect('homepage')
 
     else:
@@ -41,8 +44,13 @@ def search(request):
                 onyomi = Onyomi.objects.filter(kanji_key_id=kanji.id)
                 kunyoumi = Kunyoumi.objects.filter(kanji_key_id=kanji.id)
                 meanings = Meanings.objects.filter(kanji_key_id=kanji.id)
-                print(onyomi)
-                return render(request, 'home/search.html', {"kanji":kanji, "onyomi":onyomi, "kunyoumi":kunyoumi, "meanings":meanings})
+                context_dictionary = {
+                    "kanji":kanji, 
+                    "onyomi":onyomi, 
+                    "kunyoumi":kunyoumi, 
+                    "meanings":meanings
+                }
+                return render(request, 'home/search.html', context_dictionary)
             except IndexError as e:
                 logging.error(e)
                 return render(request, 'home/search.html')
@@ -51,61 +59,36 @@ def search(request):
             return render(request, 'home/search.html') 
     else:
         return render(request, 'home/search.html')
-    
-
-def search_dummy(request):
-    if request.method == 'POST':
-        try:
-            kanji = request.POST['kanji']
-            try:
-                kanji = Kanji.objects.filter(kanji=kanji).values()
-                id = kanji[0]["id"]
-                character = kanji[0]['kanji']
-                stroke_count = kanji[0]['stroke_count']
-                grade = kanji[0]['grade']
-                frequency = kanji[0]['frequency']
-                grade = kanji[0]['grade']
-                jlpt = kanji[0]['jlpt']
-                onyomi_list = []
-                kunyoumi_list = []
-                meanings_list = []
-                onyomi = Onyomi.objects.filter(kanji_key_id=id).values()
-                for item in onyomi:
-                    onyomi_list.append(item['character'])
-                kunyoumi = Kunyoumi.objects.filter(kanji_key_id=id).values()
-                for item in kunyoumi:
-                    kunyoumi_list.append(item['character'])
-                meanings = Meanings.objects.filter(kanji_key_id=id).values()
-                for item in meanings:
-                    meanings_list.append(item['character'])     
-                return render(request, 'home/search.html', {'stroke_count': stroke_count, "grade": grade, "frequency": frequency, "kanji": character, "jlpt": jlpt, "onyomi": onyomi_list, "kunyoumi": kunyoumi_list, "meanings": meanings_list})
-            
-            except IndexError as e:
-                logging.error(e)
-                return render(request, 'home/search.html')
-        
-        except TypeError:
-            return render(request, 'home/search.html')
-        
-    else:
-        return render(request, 'home/search.html')
 
 
 def homepage(request):
-    display_data = get_posts()
+    display_data = []
+    posts = get_posts_by_creation()
+    for post in posts:
+        # Use this kanji_id to query all associated data
+        kanji = Kanji.objects.get(kanji=post.kanji)
+        display_data.append({'post_id': post.id,
+                             'kanji': post.kanji, 
+                             'onyomi':[a.character for a in Onyomi.objects.filter(kanji_key_id=kanji.id)], 
+                             'meanings':[a.character for a in Meanings.objects.filter(kanji_key_id=kanji.id)], 
+                             'kunyoumi': [a.character for a in Kunyoumi.objects.filter(kanji_key_id=kanji.id)], 
+                             'stroke_count':kanji.stroke_count, 
+                             'mnemonic':post.mnemonic, 
+                             'posted_by':post.poster_id,
+                             'upvotes':post.upvotes})
     if request.user.is_authenticated:
         return render(request, 'home/homepage.html', {'display_data': display_data, "authenticated": True, "user": request.user})
     else:
         return render(request, 'home/homepage.html', {'display_data': display_data, "authenticated": False})
         
 
-@login_required(login_url='login')
 def update_likes(request):
     if request.user.is_authenticated:
         user_id = User.objects.get(username=request.user)
         post_id = Post.objects.get(id=request.POST['postid'][0])
         is_liked = Likes.objects.filter(user=user_id.id, post=post_id.id)
         if is_liked:
+            # If the user has already liked a particular post
             print("Already Liked!")
             like_update = Likes.objects.get(user=user_id, post=post_id)
             like_update.delete ()
@@ -113,8 +96,8 @@ def update_likes(request):
             post.upvotes -= 1
             post.save()
             return JsonResponse({"upvotes": post.upvotes})
-        
         else:
+            # If the user is liking the particular post for the first time
             like_update = Likes(user=user_id, post=post_id)
             like_update.save()
             post = Post.objects.get(id=int(request.POST['postid'][0]))
@@ -122,10 +105,15 @@ def update_likes(request):
             post.save()
             return JsonResponse({"upvotes": post.upvotes})
     else:
-        return redirect('login')
+        return JsonResponse({})
 
 
-def get_posts():
+def get_posts_by_creation():
+    """Queries posts by filtering with created date
+
+    Returns:
+        posts: Returns an objects containing all post data
+    """
     posts = Post.objects.all().order_by('-created')
     return posts
 
